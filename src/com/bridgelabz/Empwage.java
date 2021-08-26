054d40fdc9c5a2f454ccdab5e5fbf71d72229773
vamsikrish-A <krishnavamsi165@gmail.com>
2021-08-26 22:04:51 +0530

    Random checkk

--- src/com/bridgelabz/Empwage.java
+++ src/com/bridgelabz/Empwage.java
@@ -3,5 +3,11 @@ package com.bridgelabz;
 public class Empwage {
     public static void main(String[] args) {
         System.out.println("Welcomr to Employee Wage.");
+
+        double Checkout = Math.floor(Math.random() * 10) % 2;
+        if (Checkout == 1)
+            System.out.println("Employee Present.");
+        else
+            System.out.println("Employee absent.");
     }
 }
