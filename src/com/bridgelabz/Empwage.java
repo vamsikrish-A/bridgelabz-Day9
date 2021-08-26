b6eb355ad1fb1fb9e701cf0ff150528c87329809
vamsikrish-A <krishnavamsi165@gmail.com>
2021-08-26 22:15:32 +0530

    part time added

--- src/com/bridgelabz/Empwage.java
+++ src/com/bridgelabz/Empwage.java
@@ -4,16 +4,28 @@ public class Empwage {
 
     public static final int WAGE_PER_HOUR = 20;
     public static final int FULL_DAY_HOUR = 8;
+    public static final int PART_TIME_HOURS = 4;
 
     public static void main(String[] args) {
         System.out.println("Welcomr to Employee Wage.");
 
-        double Checkout = Math.floor(Math.random() * 10) % 2;
-        if (Checkout == 1) {
+        int partTime = 1;
+        int fullTIme = 2;
+        int empHours = 0;
+
+        double Checkout = Math.floor(Math.random() * 10) % 3;
+        if (Checkout == fullTIme) {
             System.out.println("Employee Present.");
-            int dailyWage = FULL_DAY_HOUR * WAGE_PER_HOUR;
-            System.out.println("Daily employee Wage::" + dailyWage);
-        } else
+            empHours = FULL_DAY_HOUR;
+        } else if (Checkout == partTime){
+            System.out.println("Part-time Employee.");
+            empHours = PART_TIME_HOURS;
+        } else {
             System.out.println("Employee absent.");
+            empHours = 0;
+        }
+
+        int dailyWage = empHours * WAGE_PER_HOUR;
+        System.out.println("Daily employee Wage::" +dailyWage);
     }
 }
